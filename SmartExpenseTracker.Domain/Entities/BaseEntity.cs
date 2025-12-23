abe9f81744bcb6ae281164c38287200b6ebadfce
vishva-patel96 <1996vishva@gmail.com>
2025-12-23 08:16:09 -0500

    Add BaseEntity and Expense entities; update project structure
    Added abstract BaseEntity and Expense entity classes to the Domain.Entities namespace to support code reuse and encapsulate expense data. Updated the project file to include folders for Enums, Interfaces, and Events to improve code organization.

---
+++ SmartExpenseTracker.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartExpenseTracker.Domain.Entities
+{
+    //create a abstract class --> cannot be used to create objects directly
+    //need to be inherited by other classes -- follow DRY principle
+    public abstract class BaseEntity
+    {
+        //get --> anyone can read the value
+        //protected set --> only this class and derived classes can change the value
+        public int Id { get; protected  set; }
+    }
+}
---
+++ SmartExpenseTracker.Domain/Entities/Expense.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartExpenseTracker.Domain.Entities
+{
+    // Expense entity inheriting from BaseEntity
+    public class Expense : BaseEntity
+    {
+        //get --> anyone can read these values
+        //Private set --> values can only change through the constructor
+        public decimal Amount { get; private set; }
+        public DateTime Date { get; private set; }
+        public int CategoryID { get; private set; }
+
+        // created immutable obj
+        //created constructor
+   
+        public Expense(decimal amount,DateTime date, int categoryID)
+        {
+            if(amount <= 0)
+            {
+                throw new Exception("Amount must be greater than zero");
+            }
+            else
+            {
+                Amount = amount;
+                Date = date;
+                CategoryID = categoryID;
+            }
+        }
+    }
+}
