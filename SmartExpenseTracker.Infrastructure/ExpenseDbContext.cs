e6ecd7e5179cd536fa4c5f22fdc64cf3d1e6e258
vishva-patel96 <1996vishva@gmail.com>
2026-01-05 08:50:55 -0500

    created new ExpenseRepository for data store. and also added ExpenseDbContext class.

---
+++ SmartExpenseTracker.Infrastructure/ExpenseDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using SmartExpenseTracker.Domain.Entities;
+
+namespace SmartExpenseTracker.Infrastructure
+{
+    public class ExpenseDbContext : DbContext
+    {
+        //constructor to pass options to base DbContext class
+        public DbSet<Expense> Expenses { get; set; }
+        //DbSet for Expense entity
+        public ExpenseDbContext(DbContextOptions<ExpenseDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ SmartExpenseTracker.Infrastructure/Repositories/ExpenseRepository.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartExpenseTracker.Domain.Entities;
+using SmartExpenseTracker.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+//expense repository implementation, so application and domain layers never touch EF Core directly
+namespace SmartExpenseTracker.Infrastructure.Repositories
+{
+    //Implementing the IExpenseRepository interface
+    public class ExpenseRepository: IExpenseRepository
+    {
+        public readonly ExpenseDbContext _context;
+        //constructor to initialize DbContext via dependency injection
+        public ExpenseRepository(ExpenseDbContext context)
+        {
+            _context = context;
+        }
+        public async Task AddAsync(Expense expense)
+        {
+            //Add the expense entity to the DbContext and Save changes
+            await _context.Expenses.AddAsync(expense);
+            await _context.SaveChangesAsync();
+        }
+        public async Task<List<Expense>> GetAllExpensesAsync()
+        {
+            //Retrieve all expenses from the DbContext as a list
+            return await _context.Expenses.AsNoTracking().ToListAsync();
+        }
+    }
+}
