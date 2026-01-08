d09e7d009d3c0220209f4767f79384457adc4dde
vishva-patel96 <1996vishva@gmail.com>
2026-01-08 07:15:43 -0500

    added new entity called Category.cs file.

---
+++ SmartExpenseTracker.Domain/Entities/Category.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartExpenseTracker.Domain.Entities
+{
+    public class Category : BaseEntity
+    {
+        public string Name { get; private set; }
+
+        protected Category() { }
+
+        public Category(string name)
+        {
+            if(string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("Category name cannot be null or empty.", nameof(name));
+            }
+            else
+            {
+                Name = name;
+            }
+        }
+
+    }
+}
